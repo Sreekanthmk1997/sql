22728fc4cbb2edeebe9b7874adce0724c18d1b33
Sreekanthmk1997 <sreekanthpes@gmail.com>
2024-08-24 19:06:34 +0530

    readme.md

---
+++ myapp/models.py
@@ -0,0 +1,16 @@
+
+
+from django.db import models
+
+class Employee(models.Model):
+    employee_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    number = models.CharField(max_length=15)
+    hire_date = models.DateField()
+    job = models.CharField(max_length=50)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    manager_id = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
