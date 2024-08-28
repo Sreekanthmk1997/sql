1b56dfd5b94167ca4a97e065b2b6317efff1dee6
Sreekanthmk1997 <sreekanthpes@gmail.com>
2024-08-28 17:47:38 +0530

    Add Django project to training2 branch

--- myapp/models.py
+++ myapp/models.py
@@ -4,13 +4,17 @@ from django.db import models
 
 class Employee(models.Model):
     employee_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
     email = models.EmailField(unique=True)
     number = models.CharField(max_length=15)
     hire_date = models.DateField()
     job = models.CharField(max_length=50)
+    department = models.CharField(max_length=50)
+
     salary = models.DecimalField(max_digits=10, decimal_places=2)
     manager_id = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.first_name
